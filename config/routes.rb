2b361248a6976f13f58bb023e51643823bf3dca3
Noriko Iida <lumos.5503@gmail.com>
2024-06-02 14:07:43 +0000

    6/2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
   get 'top' => 'homes#top'
+
   get 'books/new'
    post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
+  get 'books' => 'books#index'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
