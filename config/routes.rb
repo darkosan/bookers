bfdde3ba789c00d855b458901cf8052f613ed500
Noriko Iida <lumos.5503@gmail.com>
2024-06-06 10:28:09 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'top' => 'homes#top'
+  
+  root to:'homes#top'
   resources :books
 
-  get 'books/new'
-   post 'books' => 'books#create'
-
-  get 'books' => 'books#index'
-
-  get 'books/:id' => 'books#show'
-
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
